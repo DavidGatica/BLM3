43d3ba4ef1df3e581de78a2f4fa114d9c61537b9
nevinsantana <roll.93@hotmail.com>
2015-07-03 17:53:13 -0500

    intro
    :ok_woman:

--- index.php
+++ index.php
@@ -1,29 +1,251 @@
-<?php
-require_once('init.php');
-$loadClass = SB_Modules::loadClass('Modules_Login');
-$loginObject = new Modules_Login('5zwbvo7o26i');
-$loginObject->init();
-$loginObject->processAction();
-?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!DOCTYPE html>
+
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Best Light Mexico - Inicio</title>
-<meta name="DESCRIPTION" content="">
-<meta name="KEYWORDS" content="">
-<meta name="GENERATOR" content="Parallels Plesk Sitebuilder 4.5.0">
-</head>
-<body class="pageContent"><div style="text-align: right;">&nbsp;</div>
-<div style="text-align: center;"><html>
-<head>
-<title>Descarga</title>
-<META HTTP-EQUIV="Refresh" CONTENT="5; URL=http://www.bestlightmexico.com.mx/iluminacion">
-</head>
-<body BGCOLOR="#E0ECF8" >
+	<head>
+		<style type="text/css">
+		
+		*,
+*:after,
+*:before {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 
-<div align="center"><img src="/attachments/File/banner-luminario.gif" width='430' height='600'></div> 
-</body>
-</html>
+.switch {
+  margin: 50px auto;
+  bottom: 0;
+  right: 50%;
+  position: absolute;
+}
 
-<a href="http://www.bestlightmexico.com.mx/iluminacion"><strong>Saltar intro -&gt;</strong></a></div></body>
+.switch label {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: block;
+}
+
+.switch input {
+  top: 0; 
+  right: 0; 
+  bottom: 0; 
+  left: 0;
+  opacity: 0;
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+			#body
+			{
+				background-color: black;
+				transition: 0.7s;
+				overflow: hidden;
+			}
+			h1#especial
+			{
+				font-family: georgia;
+				position: absolute;
+				margin: 0.5em 0 0 0.4em;
+				padding: 0;
+				font-size: 300px;
+				transition: color 0.7s;
+				text-align: center;
+				color: black;
+			}
+			
+			h4#autor
+			{
+				position: absolute;
+				font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+				right: 0;
+				bottom: 0;
+				margin-right: 1em;
+				margin-bottom: 1em;
+				color: #838383;
+			}
+			
+			#nombreCompleto
+			{
+				float: right;
+				margin-top: 6.5em;
+				margin-right: 2em;
+				font-size: 65px;
+				transition: opacity 1s, margin 1s;
+				opacity: 0;
+			}		
+			
+			#movimientoLogo
+			{
+				width: 54em;
+				height: 31.5em;
+				margin: 5em auto;
+				overflow: hidden;
+			}
+			
+			#curva
+			{
+				position: absolute;
+				transition: 0.7s;
+				opacity: 0;
+			}
+			
+			
+			.switch.demo1 {
+			  width: 30px;
+			  height: 30px;
+			}
+
+			.switch.demo1 label {
+			  border-radius: 30%;
+			  background: #eaeaea;
+			  box-shadow: 
+				  0 3px 5px rgba(0,0,0,0.25),
+				  inset 0 1px 0 rgba(255,255,255,0.3),
+				  inset 0 -5px 5px rgba(100,100,100,0.1),
+				  inset 0 5px 5px rgba(255,255,255,0.3);
+			}
+
+			.switch.demo1 label:after {
+			  content: "";
+			  position: absolute;
+			  top: -8%; right: -8%; bottom: -8%; left: -8%;
+			  z-index: -1;
+			  border-radius: inherit;
+			  background: #ddd;
+			  box-shadow: 
+				inset 0 2px 1px rgba(0,0,0,0.15),
+				0 2px 5px rgba(200,200,200,0.1);
+			}
+
+			.switch.demo1 label:before {
+			  content: "";
+			  position: absolute;
+			  width: 30%;
+			  height: 30%;
+			  border-radius: 50%;
+			  left: 35%;
+			  top: 35%;
+			  background: #969696;
+			  background: radial-gradient(40% 35%, #ccc, #969696 60%);
+			  box-shadow:
+				  inset 0 2px 4px 1px rgba(0,0,0,0.3),
+				  0 1px 0 rgba(255,255,255,1),
+				  inset 0 1px 0 white;
+			}
+
+			.switch.demo1 input:checked ~ label {
+			  background: #dedede;
+			  background: -moz-linear-gradient(#dedede, #fdfdfd);
+			  background: -ms-linear-gradient(#dedede, #fdfdfd);
+			  background: -o-linear-gradient(#dedede, #fdfdfd);
+			  background: -webkit-gradient(linear, 0 0, 0 100%, from(#dedede), to(#fdfdfd));
+			  background: -webkit-linear-gradient(#dedede, #fdfdfd);
+			  background: linear-gradient(#dedede, #fdfdfd);
+			}
+
+			.switch.demo1 input:checked ~ label:before {
+			  background: #25d025;
+			  background: radial-gradient(40% 35%, #5aef5a, #25d025 60%);
+			  box-shadow:
+				  inset 0 3px 5px 1px rgba(0,0,0,0.1),
+				  0 1px 0 rgba(255,255,255,0.4),
+				  0 0 10px 2px rgba(0, 210, 0, 0.5);
+			}
+			
+			#luminaria
+			{
+				position: absolute;						
+				z-index: 2;
+				opacity: 0.5;
+				transition: 0.8s;
+			}	
+			
+			#luces
+			{
+				position: absolute;	
+				margin: 2em 0 0 10em;
+				z-index: 1;
+				transition: 0.1s;
+				opacity: 0;
+			}			
+			
+			#luminaria1
+			{
+				position: absolute;
+				width: 23em;
+				height: 20em;
+				left: 5em;
+				top: 5em;
+				margin: 0 2em;
+			}
+			
+			#flecha
+			{
+				position: absolute;
+				right: 1em;
+				top: 50%;
+				opacity: 0.5;
+				transition: 0.7s;
+			}
+			
+			#flecha:hover
+			{
+				opacity: 1;
+			}
+		</style>
+		<script>
+			function cambiaTodo()
+			{
+				if(document.getElementById('checkbox').checked)
+				{
+					document.getElementById('body').style.background = "white";
+					document.getElementById('especial').style.color = "#db6e2c";
+					document.getElementById('nombreCompleto').style.marginRight = "0";
+					document.getElementById('nombreCompleto').style.opacity = "1";
+					document.getElementById('curva').style.opacity = "1";
+					document.getElementById('curva').style.transition = "0.7s";
+					document.getElementById('luces').style.opacity = "1";
+					document.getElementById('luces').style.transition = "0.7s";
+					document.getElementById('luminaria').style.opacity = "1";
+					document.getElementById('switch').style.opacity = "1";
+				}
+				else
+				{					
+					document.getElementById('especial').style.color = "black";
+					document.getElementById('body').style.background = "black";
+					document.getElementById('nombreCompleto').style.marginRight = "4em";
+					document.getElementById('nombreCompleto').style.opacity = "0";
+					document.getElementById('curva').style.opacity = "0";
+					document.getElementById('curva').style.transition = "4s";
+					document.getElementById('luces').style.opacity = "0";
+					document.getElementById('luces').style.transition = "3s";
+					document.getElementById('luminaria').style.opacity = "0.5";
+					document.getElementById('switch').style.opacity = "0.5";
+				}
+			}
+		</script>
+	</head>
+    <body id="body">
+		<div id="movimientoLogo">
+			<div id="curva">
+				<img src="curva.png" />				
+			</div>
+			<h1 id="especial">BLM</h1>
+			<h3 id="nombreCompleto">Best Light M&eacute;xico</a></h3>
+		</div>
+		<a href="iluminacion/index.php"><img id="flecha" src="flecha.png" /></a>
+		<div id="luminaria1">
+			<img id="luminaria" src="Luminaria1.png" />
+			<img id="luces" src="brillito.png" />
+		</div>
+		<div class="switch demo1">
+			<input id="checkbox" type="checkbox" onchange="cambiaTodo()">
+			<label></label>
+		</div>		
+	</body>
 </html>
